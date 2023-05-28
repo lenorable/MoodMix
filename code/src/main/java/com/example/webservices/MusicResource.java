c4cd60a9a2163d179ca794b797221fbfe84e3c06
lenorable <70905029+lenorable@users.noreply.github.com>
2023-05-28 22:54:13 +0200

    fix and some html code

---
+++ code/src/main/java/com/example/webservices/MusicResource.java
@@ -0,0 +1,26 @@
+package com.example.webservices;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/music")
+public class MusicResource {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON) //alleen als je ook iets terug stuurt
+    public Response getShoppers() {
+        return Response.ok().build();
+    }
+
+    @POST
+    // @Consumes(MediaType.APPLICATION_JSON) //alleen als je paramameters of iets in die richting op vraag lmao?
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addShopingList() {
+        return Response.ok().build();
+    }
+}
--- code/src/test/java/com/example/model/AlgemeneTest.java
+++ code/src/test/java/com/example/model/AlgemeneTest.java
@@ -3,7 +3,6 @@ package com.example.model;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Test;
 
